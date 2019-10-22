1dc4364f920ee3535f9a0665c9a4db755ee8151b
chessai <chessai1996@gmail.com>
2019-10-21 22:10:13 -0400

    dont pin

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { }:
 
 let
-  haskell = import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/13f7dbe3f330e72c9af48845bb680d50fbd7e055.tar.gz) { };
+  haskell = import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz) { };
 
   pkgSet = haskell.mkCabalProjectPkgSet {
     plan-pkgs = import ./pkgs.nix;
